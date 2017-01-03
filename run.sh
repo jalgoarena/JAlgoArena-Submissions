7a504882a270b16a12331fbb9491d20949bc6622
Jacek Spólnik <jacek.spolnik@gmail.com>
2017-01-03 15:50:52 +0100

    Code improvements - instead of using api gateway - use eureka to find url for Auth Service - use nohup to run process in background

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+
 EUREKA_URL=http://localhost:5000/eureka/
-java -Dserver.port=5003 -Djalgoarena.apiGatewayUrl=http://localhost:5001/ -jar jalgoarena-submissions-*.jar
+PORT=5003
+nohup java -jar jalgoarena-submissions-*.jar &
--- src/main/kotlin/com/jalgoarena/web/UsersClient.kt
+++ src/main/kotlin/com/jalgoarena/web/UsersClient.kt
@@ -1,17 +1,20 @@
 package com.jalgoarena.web
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.jalgoarena.ApiGatewayConfiguration
 import com.jalgoarena.domain.User
+import com.netflix.discovery.EurekaClient
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import org.springframework.stereotype.Service
 import javax.inject.Inject
 
 @Service
-class UsersClient(@Inject val apiGatewayConfiguration: ApiGatewayConfiguration) {
+class UsersClient(@Inject val discoveryClient: EurekaClient) {
 
-    private fun authServiceUrl() = "${apiGatewayConfiguration.apiGatewayUrl}/auth"
+    private fun authServiceUrl(): String {
+        val instance = discoveryClient.getNextServerFromEureka("jalgoarena-auth", false)
+        return instance.homePageUrl
+    }
 
     private val httpClient = OkHttpClient()
     private val objectMapper = jacksonObjectMapper()
