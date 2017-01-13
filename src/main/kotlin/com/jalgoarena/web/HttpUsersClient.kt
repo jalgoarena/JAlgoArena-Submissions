db477920e91eced806b5fd13af522b837313a020
Jacek Spólnik <jacek.spolnik@gmail.com>
2017-01-13 08:15:50 +0100

    - replace OkHttpClient with RestTemplate, as the first is impossible to mock - add unit tests for GET /api/user endpoint

--- src/main/kotlin/com/jalgoarena/web/HttpUsersClient.kt
+++ src/main/kotlin/com/jalgoarena/web/HttpUsersClient.kt
@@ -1,54 +1,48 @@
 package com.jalgoarena.web
 
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.jalgoarena.domain.User
 import com.netflix.discovery.EurekaClient
-import okhttp3.OkHttpClient
-import okhttp3.Request
 import org.slf4j.LoggerFactory
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
 import org.springframework.stereotype.Service
+import org.springframework.web.client.RestOperations
 import javax.inject.Inject
 
 @Service
-class HttpUsersClient(@Inject private val discoveryClient: EurekaClient) : UsersClient {
+class HttpUsersClient(
+        @Inject private val discoveryClient: EurekaClient,
+        @Inject private val restTemplate : RestOperations
+) : UsersClient {
 
     private val LOG = LoggerFactory.getLogger(this.javaClass)
 
-    private fun authServiceUrl(): String {
-        val instance = discoveryClient.getNextServerFromEureka("jalgoarena-auth", false)
-        return instance.homePageUrl
+    private fun authServiceUrl() =
+            discoveryClient.getNextServerFromEureka("jalgoarena-auth", false).homePageUrl
+
+
+    override fun findAllUsers() = handleExceptions(returnOnException = emptyArray()) {
+        restTemplate.getForObject(
+                "${authServiceUrl()}/users", Array<User>::class.java)!!
     }
 
-    private val httpClient = OkHttpClient()
-    private val objectMapper = jacksonObjectMapper()
-
-    override fun findAllUsers(): Array<User> {
-
-        val request = Request.Builder()
-            .url("${authServiceUrl()}/users")
-            .build()
-
-        return try {
-            val response = httpClient.newCall(request).execute()
-            objectMapper.readValue(response.body().string(), Array<User>::class.java)
-        } catch (e: Exception) {
-            LOG.error(e.message)
-            emptyArray()
+    override fun findUser(token: String) = handleExceptions(returnOnException = null) {
+        val headers = HttpHeaders().apply {
+            set("X-Authorization", token)
         }
+
+        val entity = HttpEntity<HttpHeaders>(headers)
+
+        val response = restTemplate.exchange(
+                "${authServiceUrl()}/api/user", HttpMethod.GET, entity, User::class.java)
+        response.body
     }
 
-    override fun findUser(token: String): User? {
-        val request = Request.Builder()
-                .url("${authServiceUrl()}/api/user")
-                .addHeader("X-Authorization", token)
-                .build()
-
-        return try {
-            val response = httpClient.newCall(request).execute()
-            objectMapper.readValue(response.body().string(), User::class.java)
-        } catch (e: Exception) {
-            LOG.error(e.message)
-            null
-        }
+    private fun <T> handleExceptions(returnOnException: T, body: () -> T) = try {
+        body()
+    } catch(e: Exception) {
+        LOG.error("Error in querying jalgoarena auth service", e)
+        returnOnException
     }
 }
---
+++ src/test/kotlin/com/jalgoarena/web/HttpUsersClientSpec.kt
@@ -0,0 +1,62 @@
+package com.jalgoarena.web
+
+import com.jalgoarena.domain.User
+import com.netflix.appinfo.InstanceInfo
+import com.netflix.discovery.EurekaClient
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import org.mockito.BDDMockito.given
+import org.mockito.Mockito.mock
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
+import org.springframework.http.ResponseEntity
+import org.springframework.web.client.RestOperations
+
+class HttpUsersClientSpec {
+
+    private val DUMMY_TOKEN = "Bearer sdaw9awdw"
+    private val USER_ROLE = "USER"
+    private val USERS_SERVICE_DUMMY_URL = "http://localhost:9999"
+
+    private val discoveryClient = mock(EurekaClient::class.java)
+    private val restTemplate = mock(RestOperations::class.java)
+    private val usersClient = HttpUsersClient(discoveryClient, restTemplate)
+
+    @Test
+    fun delegates_get_user_request_to_auth_service_endpoint() {
+        givenDiscoveryService()
+
+        val headers = HttpHeaders().apply {
+            set("X-Authorization", DUMMY_TOKEN)
+        }
+
+        val entity = HttpEntity<HttpHeaders>(headers)
+
+
+        given(restTemplate.exchange(
+                "$USERS_SERVICE_DUMMY_URL/api/user", HttpMethod.GET, entity, User::class.java
+        )).willReturn(ResponseEntity.ok(USER))
+
+        val user = usersClient.findUser(DUMMY_TOKEN)
+
+        assertThat(user).isEqualTo(USER)
+    }
+
+    @Test
+    fun returns_null_if_there_is_any_issue_with_response() {
+        givenDiscoveryService()
+
+        val user = usersClient.findUser(DUMMY_TOKEN)
+        assertThat(user).isNull()
+    }
+
+    private fun givenDiscoveryService() {
+        val instanceInfo = mock(InstanceInfo::class.java)
+
+        given(instanceInfo.homePageUrl).willReturn(USERS_SERVICE_DUMMY_URL)
+        given(discoveryClient.getNextServerFromEureka("jalgoarena-auth", false)).willReturn(instanceInfo)
+    }
+
+    private val USER = User("mikolaj", "Kraków", "Tyniec Team", USER_ROLE, "0-0")
+}
