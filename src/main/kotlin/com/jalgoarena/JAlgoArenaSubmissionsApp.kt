d7bc07af9b16c1eef2f765e95603a4653b3d8394
Jacek Spólnik <jacek.spolnik@gmail.com>
2017-02-06 18:27:59 +0100

    - new feature - enable caching - updating spring boot to 1.5.1

--- src/main/kotlin/com/jalgoarena/JAlgoArenaSubmissionsApp.kt
+++ src/main/kotlin/com/jalgoarena/JAlgoArenaSubmissionsApp.kt
@@ -2,10 +2,12 @@ package com.jalgoarena
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cache.annotation.EnableCaching
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCaching
 open class JAlgoArenaSubmissionsApp
 
 fun main(args: Array<String>) {
