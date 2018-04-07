254b3e3ea41b0d0d3f7c5ba65a001dc206496e03
Jacek Spólnik <jacek.spolnik@gmail.com>
2018-04-07 15:47:53 +0200

    - remove dependency to particular port (it's dynamically resolved via Eureka)

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-EUREKA_URL=http://localhost:5000/eureka/ PORT=5003 nohup java -jar jalgoarena-submissions-*.jar &
+EUREKA_URL=http://localhost:5000/eureka/ nohup java -jar jalgoarena-submissions-*.jar &
