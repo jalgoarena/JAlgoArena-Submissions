c5f6d1e0e0560bdf09758b551b3d7b5858f592a8
Jacek Spólnik <jacek.spolnik@gmail.com>
2018-04-11 06:29:48 +0200

    - configure travis

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-EUREKA_URL=http://localhost:5000/eureka/ nohup java -jar jalgoarena-submissions-*.jar &
+BOOTSTRAP_SERVERS="localhost:9092,localhost:9093,localhost:9094" EUREKA_URL=http://localhost:5000/eureka/ nohup java -jar jalgoarena-submissions-*.jar &
