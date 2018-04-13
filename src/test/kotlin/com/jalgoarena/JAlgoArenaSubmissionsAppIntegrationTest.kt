b82408396ad8e419e88b4a3857b0e1f3524571a7
Jacek Spólnik <jacek.spolnik@gmail.com>
2018-04-13 11:55:16 +0200

    - fix integration test - fix travis config

--- src/test/kotlin/com/jalgoarena/JAlgoArenaSubmissionsAppIntegrationTest.kt
+++ src/test/kotlin/com/jalgoarena/JAlgoArenaSubmissionsAppIntegrationTest.kt
@@ -3,6 +3,7 @@ package com.jalgoarena
 import com.jalgoarena.data.SubmissionsRepository
 import com.netflix.discovery.EurekaClient
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.After
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
@@ -12,10 +13,11 @@ import org.springframework.test.context.junit4.SpringRunner
 import javax.inject.Inject
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.kafka.test.context.EmbeddedKafka
+import org.springframework.kafka.test.rule.KafkaEmbedded
 
 @RunWith(SpringRunner::class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-@EmbeddedKafka(partitions = 1, topics = ["results", "submissions"])
+@EmbeddedKafka(partitions = 1, topics = ["results", "submissions"], controlledShutdown = true)
 class JAlgoArenaSubmissionsAppIntegrationTest {
     @Inject
     private lateinit var restTemplate: TestRestTemplate
@@ -26,9 +28,17 @@ class JAlgoArenaSubmissionsAppIntegrationTest {
     @MockBean
     private lateinit var eurekaClient: EurekaClient
 
+    @Inject
+    private lateinit var embeddedKafka: KafkaEmbedded
+
     @Test
     fun check_if_spring_configuration_works_properly() {
         val body = this.restTemplate.getForObject("/info", String::class.java)
         assertThat(body).isEqualTo("{}")
+    }
+
+    @After
+    fun tearDown() {
+        embeddedKafka.after()
     }
 }
